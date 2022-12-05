43b2f5b12036dac4f34ba2cf28e8eacdd8286883
Nikhil vishwakarma <105054486+nikhivishwa@users.noreply.github.com>
2022-12-05 19:24:25 +0530

    Create txt.py

---
+++ helper/txt.py
@@ -0,0 +1,56 @@
+# print text for TG Users. 
+
+class mr(object):
+    PROGRESS_BAR = """\n
+╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
+┣⪼ 🗂️ : {1} | {2}
+┣⪼ ⏳️ : {0}%
+┣⪼ 🚀 : {3}/s
+┣⪼ ⏱️ : {4}
+╰━━━━━━━━━━━━━━━➣ """
+
+    ABOUT_TXT = """
+╭───────────⍟
+├🤖 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
+├👑 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁𝚂 : <a href=https://t.me/tech_AI_bots/8>𝚃𝙴𝙲𝙷 𝙰𝙸 𝙱𝙾𝚃𝚂</a> 
+├👨‍💻 𝙿𝚁𝙾𝙶𝚁𝙰𝙼𝙴𝚁 : <a href=https://t.me/curobotz>𝙲𝚞𝚛𝚘𝙱𝚘𝚝𝚣</a>
+├📕 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : <a href=https://github.com/pyrogram>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
+├✏️ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : <a href=https://www.python.org>𝙿𝚈𝚃𝙷𝙾𝙽 3</a>
+├💾 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : <a href=https://cloud.mongodb.com>𝙼𝙾𝙽𝙶𝙾𝙳𝙱</a>
+├🌀 𝙼𝚈 𝚂𝙴𝚁𝚅𝙴𝚁 : <a href=https://dashboard.koyeb.com>𝙺𝚘𝚢𝚎𝚋</a>
+├📊 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚄𝚂 : v3.6.8 [ 𝙼𝙰𝙹𝙾𝚁 ]              
+╰───────────────⍟
+                                """
+    HELP_TXT = """
+🌌 <b><u>HOW TO SET THUMBNILE</u></b>
+  
+•> /start a bot and send any picture to automatically set thumbnile.
+•> /delthumb use this command and delete your old thumbnile.
+•> /viewthumb use this command view your current thumbnile.
+
+📑 <b><u>HOW TO SET CUSTOM CAPTION</u></b>
+•> /set_caption - set a custom caption
+•> /see_caption - see your custom caption
+•> /del_caption - delete custom caption
+
+Example:- /set_caption 📕 File Name: {filename}
+💾 Size: {filesize}
+⏰ Duration: {duration}
+
+✏️ <b><u>HOW TO RENAME A FILE</u></b>
+•> send any file <br>
+•> click rename option <br>
+•> type new file name and \n send 
+•> select [ document, video, audio ]👈 choice this.
+
+
+ℹ️ Any Other Help Contact :- 
+<a href=https://t.me/+WyQN-XIUKUU1Mzk1>🛡𝗝𝗼𝗶𝗻 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 𝗚𝗿𝗼𝘂𝗽</a>
+"""
+
+#⚠️ don't remove our credits 🙏😢😢
+    DEV_TXT = """
+<b><u>Special Thanks & Developers</b></u> 
+
+» <a href=https://t.me/tech_AI_bots/9> 𝘿𝙚𝙫𝙚𝙡𝙤𝙥𝙚𝙧𝙨 𝙈𝙚𝙣𝙩𝙞𝙤𝙣 𝙃𝙚𝙧𝙚</a>
+"""
